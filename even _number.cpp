bde52d801fb9c4319e79d54d591180a16d54d5e7
Neeraj Kumar <148259510+neerajkumarroy@users.noreply.github.com>
2023-11-10 13:17:08 +0530

    oo

---
+++ even _number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a ;
+cin>>a;
+if(a%2 == 0)  // if remainder is zero then even number
+    cout<<”even”;
+else       
+    cout<<”odd”;
+return 0;
+}
+
+// output: 
+           
+Input: 8
+Output: even
