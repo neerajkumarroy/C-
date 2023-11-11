361951eb9ab05efd431b00dacacb267ee275b008
Neeraj Kumar <148259510+neerajkumarroy@users.noreply.github.com>
2023-11-11 21:39:18 +0530

    Armstrong_Number
    Armstrong number is a number that is equal to the sum of cubes of its digits. For example 0, 1, 153, 370, 371 and 407 are the Armstrong numbers

---
+++ Armstrong.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int countDigits(int num) {
+    int count = 0;
+    while (num != 0) {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
+bool isArmstrong(int num) {
+    int originalNum = num;
+    int sum = 0;
+    int power = countDigits(num);
+
+    while (num != 0) {
+        int digit = num % 10;
+        sum += pow(digit, power);
+        num /= 10;
+    }
+
+    return (sum == originalNum);
+}
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (isArmstrong(number)) {
+        cout << number << " is an Armstrong number." << endl;
+    } else {
+        cout << number << " is not an Armstrong number." << endl;
+    }
+
+    return 0;
+}
+
