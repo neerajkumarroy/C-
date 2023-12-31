f907f94362931d5a5bf2c7580a40f8707bc45e60
Neeraj Kumar <148259510+neerajkumarroy@users.noreply.github.com>
2023-12-31 19:28:52 +0530

    for_loop

---
+++ forloop.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i = 1 ,num = 1;
+	cout<<"Enter Two Numbers:";
+	cin >> num;
+	
+	for(i = 1;i <= 10; i++)
+	{
+		int table = i*num;
+		cout<<num<<"*"<<i<<"="<<table<<endl;
+	}
+	return 0;
+}
+
+Output:
+	____________________________________________________________________________
+	5*1=5
+	5*2=10
+	5*3=15
+	5*4=20
+	5*5=25
+	5*6=30
+	5*7=35
+	5*8=40
+	5*9=45
+	5*10=50
